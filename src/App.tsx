832aa72ad8f60fcfd7900bb20fb706131e6b57fd
Demis Rincon <darmfma@gmail.com>
2025-04-16 09:39:40 -0600

    fix(UI): Background color correction

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,14 @@ import ExpenseTracker from "./components/expense-tracker";
 
 const App = () => {
   return (
-    <div className="h-screen w-screen flex flex-col justify-start items-center">
+    <div className="h-screen w-screen flex flex-col justify-start items-center bg-white ">
       <header className="h-16 w-full flex justify-center items-center md:h-1/12">
         <h1 className="text-3xl font-bold md:text-5xl">Cat Expenses Web</h1>
       </header>
-      <main className="flex-grow w-full overflow-y-scroll md:h-11/12">
+      <main className="flex-grow w-full overflow-y-scroll md:h-11/12 z-1">
         <ExpenseTracker />
       </main>
-      <div className="absolute top-0 left-0 h-screen w-full overflow-x-hidden -z-10">
+      <div className="absolute top-0 left-0 h-screen w-full overflow-x-hidden z-0">
         <CatWalkingAnimation flip duration={20} startX={-100} />
         <CatWalkingAnimation duration={12} />
         <CatWalkingAnimation flip duration={25} />
