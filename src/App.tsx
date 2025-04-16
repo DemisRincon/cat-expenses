e3e53f3e66d721a9a4e26ccfb90804e62d3026b8
Demis Rincon <darmfma@gmail.com>
2025-04-15 20:24:16 -0600

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+const App = () => {
+  return (
+    <main className=" h-screen w-screen flex flex-col justify-start items-center">
+      <h1 className="text-5xl">HELLO INITIALIZATION</h1>
+    </main>
+  );
+};
+
+export default App;
